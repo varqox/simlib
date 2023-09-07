2b695599d6ed7f1f4863ca9c84641f4959a21ffe
Krzysztof Małysa <varqox@gmail.com>
2023-09-07 10:45:41 +0200

    add: sim/judge/language_suite/pascal: Pascal - a Pascal language judge suite

---
+++ include/simlib/sim/judge/language_suite/pascal.hh
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <optional>
+#include <simlib/file_path.hh>
+#include <simlib/sandbox/sandbox.hh>
+#include <simlib/sim/judge/language_suite/fully_compiled_language.hh>
+#include <simlib/slice.hh>
+#include <string_view>
+
+namespace sim::judge::language_suite {
+
+class Pascal final : public FullyCompiledLanguage {
+    std::string_view edition_str;
+
+    [[nodiscard]] sandbox::Result run_compiler(
+        Slice<std::string_view> extra_args,
+        std::optional<int> compilation_errors_fd,
+        Slice<sandbox::RequestOptions::LinuxNamespaces::Mount::Operation> mount_ops,
+        const CompileOptions& options
+    );
+
+public:
+    explicit Pascal();
+
+protected:
+    sandbox::Result is_supported_impl(const CompileOptions& options) final;
+
+    sandbox::Result compile_impl(
+        FilePath source,
+        FilePath executable,
+        int compilation_errors_fd,
+        const CompileOptions& options
+    ) final;
+};
+
+} // namespace sim::judge::language_suite
